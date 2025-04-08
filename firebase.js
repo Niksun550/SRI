1588ebf7702c435e178a8174793540ce0ad5b5cf
Nikhl V. Sawant <95631505+Niksun550@users.noreply.github.com>
2025-04-08 20:51:02 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAkwiKjPYsv4LlRQLTxH6kj15xlCnvoXKA",
+  authDomain: "testmenu-4ed51.firebaseapp.com",
+  projectId: "testmenu-4ed51",
+  storageBucket: "testmenu-4ed51.firebasestorage.app",
+  messagingSenderId: "98263305480",
+  appId: "1:98263305480:web:024158941cb5f53225f0bd",
+  measurementId: "G-CX0JDH1RDP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
